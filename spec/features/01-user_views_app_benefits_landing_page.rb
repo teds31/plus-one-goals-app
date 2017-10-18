338ecb5689e0fb25851d9225959851869c3d07ec
Teddy Stanowski <tstanowski31@gmail.com>
2017-10-18 10:04:54 -0700

    write test framework

---
+++ spec/features/01-user_views_app_benefits_landing_page.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'user views app features and benefits' do
+  #User Story
+  #As a prospective user, I want to learn more
+  #about the features and benefits of the app
+  #so I can set and achieve my goals
+
+  #Acceptance Criteria
+  #User visits landing/homepage
+  #User able to scroll down and see detailed grpahics and info
+  #User able to see a clickable sign up button
+
+  scenario 'user visits landing page' do
+
+  end
+
+  scenario 'user sees featurs and benefits' do
+
+  end
+
+  scenario 'user sees clickable sign up button' do
+    
+  end
+end
