8beb3b7a2cb572a56f0b351439a21d80fe96c7ba
Teddy Stanowski <tstanowski31@gmail.com>
2017-11-21 17:58:23 -0800

    first commit

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,5 +1,5 @@
 module DeviseHelper
   def devise_error_messages!
-    ["can't be blank", "doesn't match", "Invalid email or password."]
+    ["can't be blank", "doesn't match", "Invalid email or password.", "Please fill out all forms."]
   end
 end
---
+++ spec/features/05_user_changes_password_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'user changes password on account' do
+  # User Story
+  # as an authenticated user
+  # i want to change my password
+  # to make my account more secure
+
+  # Acceptance Criteria
+  # as an authenticated user
+  # i visit my account page
+  # go to change my password forms
+  # enter it twice, then submit to save
+
+
+end
