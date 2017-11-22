72be4d183f3727311b0a604ac5a675b31e2239b0
Teddy Stanowski <tstanowski31@gmail.com>
2017-11-21 17:58:38 -0800

    write tests

--- spec/features/05_user_changes_password_spec.rb
+++ spec/features/05_user_changes_password_spec.rb
@@ -12,5 +12,39 @@ feature 'user changes password on account' do
   # go to change my password forms
   # enter it twice, then submit to save
 
+  scenario 'user visits account page after signing in' do
+    user = FactoryBot.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    click_link 'My Account'
 
+    expect(page).to have_content(user.first_name)
+    expecet(page).to have_content(user.last_name)
+  end
+
+  scenario 'user changes password' do
+    user = FactoryBot.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    click_link 'My Account'
+
+    fill_in 'Password' with: 'newPassword1'
+    fill_in 'Password confirmation' with: 'newPassword1'
+    click_button 'Update'
+
+    expect(page).to have_content('Your password has been changed successfully.')
+  end
+
+  scenario 'user does not confirm new password' do
+    user = FactoryBot.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    click_link 'My Account'
+
+    fill_in 'Password' with: 'newPassword1'
+    fill_in 'Password confirmation' with: ''
+    click_button 'Update'
+
+    expect(page).to have_content('Please fill out all forms.')
+  end
 end
