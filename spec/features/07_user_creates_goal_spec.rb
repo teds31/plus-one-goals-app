e0171da7fdb3b3a78528b61cf525962bd5aac605
Teddy Stanowski <tstanowski31@gmail.com>
2017-11-29 08:54:36 -0800

    first commit, write tests

---
+++ spec/features/07_user_creates_goal_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'user creates first goal from dashboard' do
+  # User Story
+  # as an authenticated user,
+  # i want to create my first goal,
+  # and fill out the form so i can start working on achieving my goal
+
+  # Acceptance Criteria
+  # authenticated user is taken to dashboard,
+  # clicks on create a goal button
+  # taken to a form page to start filling out the form
+  # presses submit and goal shows up in dashboard
+
+  scenario 'user visits goal creating form' do
+
+  end
+
+  scenario 'user filles out form in creating goal' do
+
+  end
+
+  scenario 'user receives error when form is not filled' do
+    
+  end
+end
