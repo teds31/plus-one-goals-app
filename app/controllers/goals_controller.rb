a1a7904d435873a52daaa836ab2e940262ee47e3
Teddy Stanowski <tstanowski31@gmail.com>
2017-12-07 13:20:26 -0800

    edit goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,3 +1,47 @@
 class GoalsController < ApplicationController
+  def index
+    @goals = Goal.all
+  end
+
+  def new
+    @goal = Goal.new
+
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.safe
+      flash[:notice] = "New goal created!"
+      redirect_to goals_path
+    else
+      flash[:alert] = "Goal not created"
+      reder :new
+  end
+
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+    if @goal.save
+      flash[:notice] = "Goal updated!"
+      @goal.update(goal_params)
+      redirect_to goals_path
+    else
+      flash[:alert] = "Goal not updated"
+      render :edit
+    end
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(
+      :wish,
+      :outcome,
+      :obstacle
+    )
+  end
   
 end
